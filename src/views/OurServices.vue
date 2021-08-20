3ca746489dfaf5ebeb3f4a902a54a2b8bc09c45b
atifkuwaitnet <info@logicsdrive.com>
2021-08-20 09:26:10 +0500

    our services page update

--- src/views/OurServices.vue
+++ src/views/OurServices.vue
@@ -1,39 +1,26 @@
 <template>
-  <div class="ourServices">
-    <base-inner-banner :banner-detail="innerBannerDetail" />
-    <div class="container">
-      <services-type></services-type>
-      <base-media-slider :mediaSlides="images"></base-media-slider>
-    </div>
-  </div>
+	<div class="ourServices">
+		<base-inner-banner :banner-detail="innerBannerDetail" />
+		<div class="container">
+			<services-type></services-type>
+		</div>
+	</div>
 </template>
 
 <script>
-import BaseMediaSlider from "@/common/components/base/BaseMediaSlider.vue";
-import ServicesType from "../modules/services/ServicesType.vue";
+import BaseMediaSlider from '@/common/components/base/BaseMediaSlider.vue';
+import ServicesType from '../modules/services/ServicesType.vue';
 export default {
-  components: { ServicesType, BaseMediaSlider },
-  data() {
-    return {
-      images: [
-        {
-          image: "shutterstock_597162353.png",
-        },
-        {
-          image: "shutterstock_597162353.png",
-        },
-        {
-          image: "shutterstock_597162353.png",
-        },
-      ],
-      innerBannerDetail: {
-        image: "BOD.png",
-        title: "Our services",
-      },
-    };
-  },
+	components: { ServicesType, BaseMediaSlider },
+	data() {
+		return {
+			innerBannerDetail: {
+				image: 'BOD.png',
+				title: 'Our services',
+			},
+		};
+	},
 };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
