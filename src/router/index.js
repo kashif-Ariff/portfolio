8525a9e3ccf3a747a7695cf786f6c4aebd773e1b
atifkuwaitnet <info@logicsdrive.com>
2021-08-25 09:17:55 -0700

    project-idenfifier upadte

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-import Vue from 'vue'
+import Vue from "vue";
 import VueRouter from "vue-router";
-import home from "@/modules/home/router"
+import home from "@/modules/home/router";
 // import covid19 from "@/modules/covid19/router"
 // import mediaCenter from "@/modules/meida-center/router"
 // import sustainability from "@/modules/sustainability/router"
@@ -8,169 +8,160 @@ import home from "@/modules/home/router"
 // import business from "@/modules/zain-buniness/router"
 
 const routes = [
-    // {
-    //     path: '/:lang',
-    //     name: 'Home',
-    //     meta: {
-    //         title: '',
-    //     },
-    //     component: () =>
-    //         import ('../views/Home.vue'),
-    // },
-    {
-        path: `/:lang`,
-        component: {
-            render(c) {
-                return c('router-view')
-            },
-        },
-        children: [...home],
+  // {
+  //     path: '/:lang',
+  //     name: 'Home',
+  //     meta: {
+  //         title: '',
+  //     },
+  //     component: () =>
+  //         import ('../views/Home.vue'),
+  // },
+  {
+    path: `/:lang`,
+    component: {
+      render(c) {
+        return c("router-view");
+      },
     },
-    {
-                path: `/:lang/media-center`,
-                name: 'media-center',
-                component: () =>
-                    import ('../views/MediaCenter.vue'),
-            },
-            {
-                path: `/:lang/board-directors`,
-                name: 'contact-us',
-                component: () =>
-                    import ('../views/BoardDirectors.vue'),
-     },
-            {
-                path: `/:lang/our-services`,
-                name: 'contact-us',
-                component: () =>
-                    import ('../views/OurServices.vue'),
-            },
-            {
-                path: `/:lang/knowledge-library`,
-                name: 'knowledge-library',
-                component: () =>
-                    import ('../views/KnowledgeLibrary.vue'),
-            },
-            {
-                path: `/:lang/support-center`,
-                name: 'support-center',
-                component: () =>
-                    import ('../views/SupportCenters.vue'),
-            },
-            {
-                path: `/:lang/initiators`,
-                name: 'initiators',
-                component: () =>
-                    import ('../views/Initiators.vue'),
-            },
-            {
-                path: `/:lang/publication`,
-                name: 'publication',
-                component: () =>
-                    import ('../views/Publication.vue'),
-            },
-            {
-                path: `/:lang/common-interest`,
-                name: 'common-interest',
-                component: () =>
-                    import ('../views/CommonInterest.vue'),
-            },
-            {
-                path: `/:lang/contact`,
-                name: 'Contact',
-                component: () =>
-                    import ('../views/Contact.vue'),
-            },
-            {
-                path: `/:lang/media-center-detail`,
-                name: 'media-center-detail',
-                component: () =>
-                    import ('../views/MediaCenterDetail.vue'),
-            },
-            {
-                path: `/:lang/faq`,
-                name: 'faq',
-                component: () =>
-                    import ('../views/Faq.vue'),
-            },
-            {
-                path: `/:lang/search`,
-                name: 'search',
-                component: () =>
-                    import ('../views/Search.vue'),
-            },
-            {
-                path: `/:lang/board-publication`,
-                name: 'board-publication',
-                component: () =>
-                    import ('../views/BoardPublication.vue'),
-            },
-            {
-                path: `/:lang/mission-vision`,
-                name: 'mission-vision',
-                component: () =>
-                    import ('../views/MissionVision.vue'),
-            },
-//     {
-//         path: `/:lang/contact-us`,
-//         name: 'contact-us',
-//         component: () =>
-//             import ('../views/ContactUs.vue'),
-//     },
-//     {
-//         path: `/:lang/media-gallery`,
-//         name: 'media-gallery',
-//         component: () =>
-//             import ('../views/MediaGallery.vue'),
-//     },
-//     {
-//         path: `/:lang/gallery-detail`,
-//         name: 'gallery-detail',
-//         component: () =>
-//             import ('../views/GalleryDetail.vue'),
-//     },
-//     {
-//         path: `/:lang/course-list`,
-//         name: 'course-list',
-//         component: () =>
-//             import ('../views/CourseList.vue'),
-//     },
-//     {
-//         path: `/:lang/our-impact`,
-//         name: 'our-impact',
-//         component: () =>
-//             import ('../views/OurImpect.vue'),
-//     },
-//     {
-//         path: `/:lang/search`,
-//         name: 'Search',
-//         component: () =>
-//             import ('../views/Search.vue'),
-//     },
-//     {
-//         path: `/:lang/cource-detail`,
-//         name: 'media-gallery',
-//         component: () =>
-//             import ('../views/CourseDetails.vue'),
-//     },
-//     {
-//           path: `/:lang/meet-mentors`,
-//           name: 'meet-mentors',
-//           component: () =>
-//           import ('../views/MeetMentors.vue'),
-//      },
-//      {
-//           path: `/:lang/test`,
-//           name: 'test',
-//           component: () =>
-//           import ('../views/Test.vue'),
-//      },
-    // {
-    //     path: `/:lang/slider`,
-    //     name: 'slider',
-    //     component: () =>
-    //         import ('../views/slider.vue'),
-    // },
-]
+    children: [...home],
+  },
+  {
+    path: `/:lang/media-center`,
+    name: "media-center",
+    component: () => import("../views/MediaCenter.vue"),
+  },
+  {
+    path: `/:lang/board-directors`,
+    name: "contact-us",
+    component: () => import("../views/BoardDirectors.vue"),
+  },
+  {
+    path: `/:lang/our-services`,
+    name: "contact-us",
+    component: () => import("../views/OurServices.vue"),
+  },
+  {
+    path: `/:lang/knowledge-library`,
+    name: "knowledge-library",
+    component: () => import("../views/KnowledgeLibrary.vue"),
+  },
+  {
+    path: `/:lang/support-center`,
+    name: "support-center",
+    component: () => import("../views/SupportCenters.vue"),
+  },
+  {
+    path: `/:lang/initiators`,
+    name: "initiators",
+    component: () => import("../views/Initiators.vue"),
+  },
+  {
+    path: `/:lang/publication`,
+    name: "publication",
+    component: () => import("../views/Publication.vue"),
+  },
+  {
+    path: `/:lang/common-interest`,
+    name: "common-interest",
+    component: () => import("../views/CommonInterest.vue"),
+  },
+  {
+    path: `/:lang/contact`,
+    name: "Contact",
+    component: () => import("../views/Contact.vue"),
+  },
+  {
+    path: `/:lang/media-center-detail`,
+    name: "media-center-detail",
+    component: () => import("../views/MediaCenterDetail.vue"),
+  },
+  {
+    path: `/:lang/faq`,
+    name: "faq",
+    component: () => import("../views/Faq.vue"),
+  },
+  {
+    path: `/:lang/search`,
+    name: "search",
+    component: () => import("../views/Search.vue"),
+  },
+  {
+    path: `/:lang/board-publication`,
+    name: "board-publication",
+    component: () => import("../views/BoardPublication.vue"),
+  },
+  {
+    path: `/:lang/mission-vision`,
+    name: "mission-vision",
+    component: () => import("../views/MissionVision.vue"),
+  },
+  {
+    path: `/:lang/project-idenfifier`,
+    name: "project-idenfifier",
+    component: () => import("../views/ProjectIdentifier.vue"),
+  },
+  //     {
+  //         path: `/:lang/contact-us`,
+  //         name: 'contact-us',
+  //         component: () =>
+  //             import ('../views/ContactUs.vue'),
+  //     },
+  //     {
+  //         path: `/:lang/media-gallery`,
+  //         name: 'media-gallery',
+  //         component: () =>
+  //             import ('../views/MediaGallery.vue'),
+  //     },
+  //     {
+  //         path: `/:lang/gallery-detail`,
+  //         name: 'gallery-detail',
+  //         component: () =>
+  //             import ('../views/GalleryDetail.vue'),
+  //     },
+  //     {
+  //         path: `/:lang/course-list`,
+  //         name: 'course-list',
+  //         component: () =>
+  //             import ('../views/CourseList.vue'),
+  //     },
+  //     {
+  //         path: `/:lang/our-impact`,
+  //         name: 'our-impact',
+  //         component: () =>
+  //             import ('../views/OurImpect.vue'),
+  //     },
+  //     {
+  //         path: `/:lang/search`,
+  //         name: 'Search',
+  //         component: () =>
+  //             import ('../views/Search.vue'),
+  //     },
+  //     {
+  //         path: `/:lang/cource-detail`,
+  //         name: 'media-gallery',
+  //         component: () =>
+  //             import ('../views/CourseDetails.vue'),
+  //     },
+  //     {
+  //           path: `/:lang/meet-mentors`,
+  //           name: 'meet-mentors',
+  //           component: () =>
+  //           import ('../views/MeetMentors.vue'),
+  //      },
+  //      {
+  //           path: `/:lang/test`,
+  //           name: 'test',
+  //           component: () =>
+  //           import ('../views/Test.vue'),
+  //      },
+  // {
+  //     path: `/:lang/slider`,
+  //     name: 'slider',
+  //     component: () =>
+  //         import ('../views/slider.vue'),
+  // },
+];
 
 // const routes = [{
 //         path: '/:lang',
@@ -293,23 +284,23 @@ const routes = [
 
 // ]
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes,
-    scrollBehavior(to, from, savedPosition) {
-        if (to.hash) {
-            return {
-                selector: to.hash,
-                behavior: 'smooth',
-            }
-        } else {
-            return new Promise((resolve, reject) => {
-                resolve({ x: 0, y: 0 })
-            })
-        }
-    },
-})
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        selector: to.hash,
+        behavior: "smooth",
+      };
+    } else {
+      return new Promise((resolve, reject) => {
+        resolve({ x: 0, y: 0 });
+      });
+    }
+  },
+});
 
-export default router
+export default router;
---
+++ src/views/ProjectIdentifier.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="projectIdentifier">
+    <base-inner-banner :banner-detail="innerBannerDetail" />
+    <div class="container">
+      <project-identifier-content />
+    </div>
+  </div>
+</template>
+
+<script>
+import ProjectIdentifierContent from "@/modules/project-identifier/ProjectIdentifierContent";
+export default {
+  components: { ProjectIdentifierContent },
+  data() {
+    return {
+      innerBannerDetail: {
+        image: "BOD.png",
+        title: "Project Scale Definitions",
+        subTitle:
+          "According To The Executive Regulations Of Law No. 14 Of 2018 Amending Some Provisions Of Law No. 98 Of 2013 Regarding The National Fund For Small And Medium Enterprises Development. ",
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
