4a960bb97357ddf1ab1e300f89501b750ca42fb6
logicsdrive <info@logicsdrive.com>
2022-01-04 17:42:10 +0500

    updates

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
         },
     },
 
-    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/portfolio' : '/',
     devServer: {
         port: 8080,
     },
