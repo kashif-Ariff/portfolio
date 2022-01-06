6d18b659e5c0f60d3bd501a3b3fbc412e59c37d6
kashif <info@logicsdrive.com>
2022-01-06 17:07:54 +0500

    message

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -6,12 +6,15 @@
           <div class="flex-row">
              
             <div class="aboutUs__left">
+              <div class="aboutUs__left--image">
+                <img src="@/assets/images/my.jpg" alt="" class="img-fluid">
+              </div>
             </div>
             <div class="aboutUs__right">
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quidem, asperiores harum eum non facere voluptatibus officiis reprehenderit exercitationem, aliquam modi placeat optio eligendi minus sed quo quam? Ut, eos!</p>
                <form>
                  <div class="button-row">
-                  <button class="btn btn-primary" href="#"><span>Download CV</span></button>
+                  <a class="btn btn-primary" href="../cv/cv.pdf" download><span>Download CV</span></a>
                 </div>
                </form>
             </div>
@@ -27,12 +30,37 @@
 <style lang="scss" scoped>
   .aboutUs{
     position:relative;
+    .flex-row{
+      max-width:1100px;
+      margin:auto;
+      align-items: center;
+    }
     &__left{
-      flex:0 0 50%;
-      max-width:50%;
+      flex:0 0 370px;
+      max-width:370px;
+      &--image{
+        position:relative;
+        border:3px solid var(--primary);
+        padding:rem(30px);
+        overflow: hidden;
+        cursor: pointer;
+        border-radius: 15px;
+        img{
+          transition: .6s ease all;
+        }
+        &:hover{
+          img{
+            transform: scale(1.2);
+          }
+        }
+      }
     }
     &__right{
       flex:1;
+      padding-left:rem(100px);
+      p{
+        max-width:500px;
+      }
     }
   }
 </style>
