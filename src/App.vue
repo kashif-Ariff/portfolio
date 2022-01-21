4a239de20803a8db8cf979f95df65fa71a7721f1
kashif <info@logicsdrive.com>
2022-01-21 17:56:53 +0500

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
 	<div id="app">
+		 <base-loader></base-loader>
 		<the-header />
 		<main role="main">
 			<router-view />
@@ -11,11 +12,13 @@
 // @ is an alias to /src
 import TheFooter from '@/common/layout/TheFooter.vue';
 import TheHeader from '@/common/layout/TheHeader.vue';
+import BaseLoader from '@/common/components/base/BaseLoader.vue';
 export default {
 	name: 'App',
 	components: {
 		TheHeader,
 		TheFooter,
+		BaseLoader,
 	},
 };
 </script>
